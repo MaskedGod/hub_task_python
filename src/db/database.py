3b9db733a673044830cf6deb288d3a980ade0d5e
MaskedGod <codegobbledygook@gmail.com>
2025-03-27 23:04:16 +0400

    feat:  database settings, database connection, migration.

---
+++ src/db/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from config.settings import settings
+
+
+engine = create_async_engine(settings.DATABASE_URL)
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_session():
+    async with async_session() as session:
+        try:
+            yield session
+        except Exception as e:
+            await session.rollback()
+            raise e
+        finally:
+            await session.close()
+
+
+class Base(DeclarativeBase):
+    pass
